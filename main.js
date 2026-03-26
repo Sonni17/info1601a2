7d75f11e7e55fe0ae4a7299dfc84271d69876f0d
Sonni17 <sonyaeasanchez@outlook.com>
2026-03-25 20:19:28 -0400

    main functions

--- main.js
+++ main.js
@@ -7,18 +7,54 @@ async function loadListing(){
 }
 
 function displayListing(pokemon){
-    //receives an array of pokemon objects and displays it on the page in #listing
+     const listing = document.getElementById("listing");
+  listing.innerHTML = "";
+ 
+  pokemon.forEach(p => {
+    const a = document.createElement("a");
+    a.href      = `#${p.name}`;
+    a.id        = p.name;
+    a.className = "collection-item";
+    a.textContent = p.name;
+    a.onclick   = () => getPokemon(p.id);
+    listing.appendChild(a);
 
-}
+});
 
 async function getPokemon(id){
-   //makes a request to the get pokemon detail API url
-   //gets the data from the request and sends it to displayPokemon()
+   currentPokemonId = pokemon_id;
+ 
+  fetch(`${BASE_URL}/pokemon/${pokemon_id}`)
+    .then(res => res.json())
+    .then(data => displayPokemon(data))
+    .catch(err => console.error("getPokemon error:", err));
 
 }
 
 function displayPokemon(pokemon){
-    //receives a pokemon object and displays its data on the page in #result
+     const result = document.getElementById("result");
+ 
+  result.innerHTML = `
+    <div id="pokemon-detail" class="card col m12 l10 offset-l1" style="margin-top: 20px">
+      <div class="card-image">
+        <img class="teal" src="${pokemon.image}" alt="${pokemon.name} Image">
+      </div>
+      <div class="card-content">
+        <span class="card-title"><p>${pokemon.name} #${pokemon.id}</p></span>
+        <p>Type1: ${pokemon.type1}</p>
+        <p>Weight: ${pokemon.weight}</p>
+        <p>Height: ${pokemon.height}</p>
+        <a onclick="catchPokemon(${pokemon.id})"
+           id="catchBtn"
+           style="position:absolute; right:15px; bottom:80px"
+           class="btn-floating btn-large waves-effect waves-light red">
+          <span class="iconify" style="font-size:40px; margin-top:8px"
+                data-icon="mdi-pokeball" data-inline="false"></span>
+        </a>
+      </div>
+    </div>
+  `;
+}
 }
 
 // Bonus Functions
